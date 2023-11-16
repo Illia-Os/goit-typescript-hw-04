46797c36378e71d746df97d127799bd0e16ae33d
Illia-Os <113521393+Illia-Os@users.noreply.github.com>
2023-11-16 03:43:34 +0200

    new

--- src/homework/3.tsx
+++ src/homework/3.tsx
@@ -1,13 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, ChangeEvent, FC } from "react";
 
-export function FormComponent() {
-  const [value, setValue] = useState("");
+interface FormComponentProps {}
 
-  const handleChange = (event) => {
+const FormComponent: FC<FormComponentProps> = () => {
+  const [value, setValue] = useState<string>("");
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     setValue(event.target.value);
   };
 
-  return (
-    <input type="text" value={value} onChange={handleChange} />
-  );
-}
+  return <input type="text" value={value} onChange={handleChange} />;
+};
+
+export default FormComponent;
